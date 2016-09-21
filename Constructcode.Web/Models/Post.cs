32e403bcd28b9b4946809ac625facaec7088fa63
Rune <runeanielsen@live.dk>
2016-09-21 21:17:36 +0200

    Renamed text to content

--- Constructcode.Web/Models/Post.cs
+++ Constructcode.Web/Models/Post.cs
@@ -6,7 +6,7 @@ namespace Constructcode.Web.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Text { get; set; }
+        public string Content { get; set; }
         public string Url { get; set; }
         public IEnumerable<string> Tags { get; set; }
         public IEnumerable<Category> Category { get; set; }
