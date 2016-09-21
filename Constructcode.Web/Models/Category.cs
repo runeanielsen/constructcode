d8db1beb3b030a5efd6c9814822f8eb6cfeb7f44
Rune <runeanielsen@live.dk>
2016-09-21 21:01:54 +0200

    Added post and category to solution

---
+++ Constructcode.Web/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Constructcode.Web.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<Post> Posts { get; set; }
+    }
+}
---
+++ Constructcode.Web/Models/Post.cs
@@ -0,0 +1,12 @@
+﻿namespace Constructcode.Web.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string Url { get; set; }
+        public string[] Tags { get; set; }
+        public Category Category { get; set; }
+    }
+}
