2eb46986152d9481993bc8df4f41307bf635c3a6
Rune <runeanielsen@live.dk>
2016-09-22 20:43:39 +0200

    unit of work injected

--- Constructcode.Web/Controllers/HomeController.cs
+++ Constructcode.Web/Controllers/HomeController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Constructcode.Web.Core;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ConstructCode.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IUnitOfWork _context;
+
+        public HomeController(IUnitOfWork context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
