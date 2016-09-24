8489b07eb41b0be519b0bc10233d4795ddc3e573
Rune <runeanielsen@live.dk>
2016-09-24 22:09:17 +0200

    Migration to add account

--- Constructcode.Web/Core/Domain/Message.cs
+++ Constructcode.Web/Core/Domain/Message.cs
@@ -6,7 +6,6 @@
         public string Name { get; set; }
         public string Content { get; set; }
 
-        public int PostId { get; set; }
-        public virtual Post Post { get; set; }
+        public Post Post { get; set; }
     }
 }
