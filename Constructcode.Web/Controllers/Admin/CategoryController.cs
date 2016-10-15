1af330f0f7661d2445f89cfe00b7837053a99372
Rune <runeanielsen@live.dk>
2016-10-15 11:31:58 +0200

    Create post and edit post now uses a shared layout

---
+++ Constructcode.Web/Controllers/Admin/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Constructcode.Web.Controllers.Admin
+{
+    public class CategoryController : Controller
+    {
+        private readonly IMapper _mapper;
+
+        public CategoryController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
