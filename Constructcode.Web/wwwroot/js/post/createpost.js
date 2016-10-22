ad9eae23f02a119815e662ea12b4a2643faf818d
Rune <runeanielsen@live.dk>
2016-10-22 23:50:25 +0200

    Init angular to solution

---
+++ Constructcode.Web/wwwroot/js/post/createpost.js
@@ -0,0 +1,11 @@
+﻿(function() {
+    angular.module("app")
+        .controller("CreatePostController", CreatePostController);
+
+    function CreatePostController() {
+        var vm = this;
+
+        vm.DisplayText = "Hello World!";
+
+    }
+})();
