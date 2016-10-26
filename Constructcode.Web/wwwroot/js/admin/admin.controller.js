29fc8744d9b472baa8166ac69428eed508ea4eac
Rune <runeanielsen@live.dk>
2016-10-26 08:50:11 +0200

    Added sanitize to solution and admin page now displays correct posts

---
+++ Constructcode.Web/wwwroot/js/admin/admin.controller.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    angular.module("app").controller("AdminController", AdminController);
+
+    function AdminController($sce, postService) {
+        var vm = this;
+
+        vm.posts = [];
+
+        function init() {
+            postService.getAllPosts().then(function(response) {
+                vm.posts = response.data;
+            });
+        }
+
+        init();
+    }
+})();
--- Constructcode.Web/wwwroot/js/app.js
+++ Constructcode.Web/wwwroot/js/app.js
@@ -1,3 +1,3 @@
 ﻿(function() {
-    angular.module("app", []);
+    angular.module("app", ["ngSanitize"]);
 })();
