675fee266a08cecc5b3beb0abc9ab339fb9bdd44
Rune <runeanielsen@live.dk>
2016-11-27 20:16:28 +0100

    Started implementation of text-editor

---
+++ Constructcode.Web/wwwroot/js/text-editor/text-editor.directive.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app')
+        .directive('textEditor', textEditor);
+
+    function textEditor() {
+        var link = function (scope) {
+            var vm = scope;
+
+            vm.insertBoldText = function () {
+                scope.content = "Hello World!";
+            }
+        }
+
+        return {
+            scope: {
+                content: '=ngModel'
+            },
+            templateUrl: '/templates/text-editor.template.html',
+            link: link
+        }
+    }
+})();
