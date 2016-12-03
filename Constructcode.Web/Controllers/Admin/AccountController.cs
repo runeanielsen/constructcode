51e0da6291450f8add94b345988216852fde8b50
Rune <runeanielsen@live.dk>
2016-12-03 13:57:53 +0100

    Added button css to solution

--- Constructcode.Web/Controllers/Admin/AccountController.cs
+++ Constructcode.Web/Controllers/Admin/AccountController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Constructcode.Web.Controllers.Admin
 {
+    [Authorize]
     public class AccountController : Controller
     {
         public IActionResult Manage()
