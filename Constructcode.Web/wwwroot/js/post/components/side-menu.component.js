24c8eedc4dce1433d40aedd6a5f412cab9fe3e18
Rune <runeanielsen@live.dk>
2016-12-12 19:29:16 +0100

    Created side-menu component

---
+++ Constructcode.Web/wwwroot/js/post/components/side-menu.component.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app').component('sideMenu', {
+        bindings: {
+            post: '='
+        },
+        templateUrl: '/js/post/components/side-menu.template.html',
+        controllerAs: 'vm',
+
+        controller: function () {
+            var vm = this;
+
+        }
+    });
+})();
