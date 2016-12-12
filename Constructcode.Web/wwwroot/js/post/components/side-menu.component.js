fb6903ccba34375e2e2976995afda4229019fe34
Rune <runeanielsen@live.dk>
2016-12-12 20:47:01 +0100

    Implemented edit, create and delete category

--- Constructcode.Web/wwwroot/js/post/components/side-menu.component.js
+++ Constructcode.Web/wwwroot/js/post/components/side-menu.component.js
@@ -25,6 +25,49 @@
                 getAllCategories();
             };
 
+            vm.createCategory = function () {
+                var categoryName = window.prompt('Insert category name', '');
+
+                if ($.trim(categoryName) === '')
+                    return;
+
+                var category = { title: categoryName };
+
+                categoryService.createCategory(category).then(function (response) {
+                    vm.categories.push(response.data);
+                }, function (response) {
+                    alert(response.data);
+                });
+            }
+
+            vm.uploadImage = function () {
+                ngDialog.open(
+                    {
+                        template: '/templates/upload-image.template.html',
+                        className: 'ngdialog-theme-default',
+                        controller: 'UploadImageController as vm'
+                    });
+            }
+
+            vm.editCategory = function (category) {
+                var categoryName = window.prompt('Insert category name', '');
+
+                if ($.trim(categoryName) === '')
+                    return;
+
+                categoryService.editCategory({ title: categoryName, id: category.id }).then(function () {
+                    category.title = categoryName;
+                }, function (response) {
+                    alert(response.data);
+                });
+            }
+
+            vm.deleteCategory = function (category) {
+                categoryService.deleteCategory(category.id).then(function () {
+                    vm.categories = vm.categories.filter(item => item !== category);
+                });
+            }
+
             function getAllCategories() {
                 categoryService.getAllCategories().then(function (response) {
                     vm.categories = response.data;
