08be963737f80ba497b611d7b2a7d3a9f38b80d7
Rune Nielsen <runeanielsen@live.dk>
2017-03-19 16:11:07 +0100

    Moved create-post to component

---
+++ Constructcode.Web/App/js/post/components/create-post.component.js
@@ -0,0 +1,35 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app')
+        .component('createPost', createPostComponent());
+
+    function createPostComponent() {
+        return {
+            templateUrl: '/templates/create-post.template.html',
+            controllerAs: 'vm',
+            controller: ['postService', 'redirectService', 'tinymceConfigService', CreatePostController]
+        }
+
+        function CreatePostController(postService, redirectService, tinymceConfigService) {
+            var vm = this;
+
+            vm.tinymceOptions = tinymceConfigService.tinymceOptions;
+
+            vm.post = {
+                title: '',
+                content: '',
+                postCategories: [],
+                created: moment(new Date()).format('DD MMMM YYYY')
+            }
+
+            vm.createPost = function () {
+                postService.createPost(vm.post).then(function () {
+                    redirectService.admin(true);
+                }, function (response) {
+                    alert(response.data);
+                });
+            }
+        }
+    }
+})();
