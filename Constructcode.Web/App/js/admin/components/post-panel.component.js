f839964e9c55da839861918ad42ccba2a9cd73b6
Rune Nielsen <runeanielsen@live.dk>
2017-03-19 15:57:34 +0100

    Moved admin index to component

---
+++ Constructcode.Web/App/js/admin/components/post-panel.component.js
@@ -0,0 +1,28 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app')
+        .component('postPanel', postPanel());
+
+    function postPanel() {
+        return {
+            templateUrl: '/templates/post-panel.template.html',
+            controllerAs: 'vm',
+            controller: ['redirectService', 'postService', postPanelController]
+        };
+
+        function postPanelController(redirectService, postService) {
+            var vm = this;
+
+            vm.posts = [];
+            vm.redirect = redirectService;
+
+            init();
+            function init() {
+                postService.getAllPosts().then(function (response) {
+                    vm.posts = response.data;
+                });
+            }
+        }
+    }
+})();
