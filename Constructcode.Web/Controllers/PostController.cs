17634dd75a63f5040ff1814c66f6e409295d4b2b
Rune A. Nielsen <runeanielsen@live.dk>
2018-09-29 17:11:31 +0200

    Cleanup

--- Constructcode.Web/Controllers/PostController.cs
+++ Constructcode.Web/Controllers/PostController.cs
@@ -4,7 +4,6 @@ using Constructcode.Web.Service;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System;
 
 namespace Constructcode.Web.Controllers
 {
